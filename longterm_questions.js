47d28e0264ca8f04f1106fa80a429a25d0a46dce
Adory Vo <adoryvo@gmail.com>
2020-08-05 14:56:55 -0700

    Responsiveness and question data retrieval/display

--- longterm_questions.js
+++ longterm_questions.js
@@ -1,9 +1,9 @@
 //VARIABLES
-let new_data;
+let longterm_data;
 
 //FUNCTIONS
-async function getNewQ() {
-    let response = await fetch("data/newboo_2.json");
+async function getLongtermQ() {
+    let response = await fetch("data/longterm.json");
     return await response.json();
 }
 
@@ -18,29 +18,29 @@ let slider = document.querySelector("#xxxsauce");
 
 //EVENT LISTENERS
 $(document).ready(async () => {
-    let new_questions = await getNewQ();
-    new_data = new_questions[2].data;
+    let longterm_questions = await getLongtermQ();
+    longterm_data = longterm_questions[2].data;
 
     //DEFAULT question
-    let random_num = await genRand(new_data.length);
-    let random_query = new_data[random_num];
+    let random_num = await genRand(longterm_data.length);
+    let random_query = longterm_data[random_num];
 
     while (random_query.xxxsauce === "1") {
-        random_num = genRand(new_data.length);
-        random_query = new_data[random_num];
+        random_num = genRand(longterm_data.length);
+        random_query = longterm_data[random_num];
     }
 
-    questions.textContent = new_data[random_num].question;
+    questions.textContent = longterm_data[random_num].question;
 });
 
 shuffle.addEventListener("click", async () => {
-    let random_num = await genRand(new_data.length);
-    let random_query = new_data[random_num];
+    let random_num = await genRand(longterm_data.length);
+    let random_query = longterm_data[random_num];
     let xxxChecked = slider.checked;
 
     while (questions.textContent === random_query.question || Boolean(parseInt(random_query.xxxsauce)) !== xxxChecked) {
-        random_num = await genRand(new_data.length);
-        random_query = new_data[random_num];
+        random_num = await genRand(longterm_data.length);
+        random_query = longterm_data[random_num];
     }
 
     questions.textContent = random_query.question;
